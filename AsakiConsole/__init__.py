d0b98a6f8e51d32cb3465931b5b7d79408fc2145
zevtyardt <apklover76@gmail.com>
2020-06-30 12:10:29 +0700

    menambahkan argumen baru

--- AsakiConsole/__init__.py
+++ AsakiConsole/__init__.py
@@ -1,5 +1,17 @@
 import sys
+import cmd2
 import os
+import argparse
+
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("commands", metavar="command", nargs="*", help="execute the command and exit.")
+    parser.add_argument("-k", "--keep-running", dest="keep_running", action="store_true", help="keep running `cmdloop`")
+    return parser
+
+parser = make_parser()
+args = parser.parse_args()
 
 sys.path.insert(0, os.path.dirname(__file__))
 from lib.lazy_loader import Loader, awesome_intro
@@ -15,9 +27,16 @@ class Asaki(Loader):
 
 def main():
     sys.argv = sys.argv[:1]
-
     flow = Asaki()
-    flow.cmdloop()
+    if args.commands:
+        for command in args.commands:
+            try:
+                flow.poutput(f"Executing command: {command}")
+                flow.onecmd(command)
+            except cmd2.exceptions.Cmd2ArgparseError:
+                pass
+    if args.keep_running or not args.commands:
+        flow.cmdloop()
 
 if __name__ == "__main__":
-    x = main()
+    main()
