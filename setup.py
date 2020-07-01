4398aa4fd9ee89c0b3363036da30b64609928548
zevtyardt <apklover76@gmail.com>
2020-07-01 12:45:21 +0700

    add app version

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import os
+from AsakiConsole.lib.constants import APP_VERSION
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup (name = "asaki",
+       version = APP_VERSION,
        author = "val (zvtyrdt.id)",
-       description="Asaki - All in one Penetration Toolkit",
+       description = "Asaki - All in one Penetration Toolkit",
        url = "https://github.com/zevtyardt",
-       install_requires=requirements,
+       install_requires = requirements,
        py_modules = ["AsakiConsole"],
        packages=find_packages(),
        include_package_data=True,
        zip_safe=False,
-       entry_points={'console_scripts': ['asaki=AsakiConsole:main']}
+       entry_points={'console_scripts': ['asaki.console=AsakiConsole.asaki:main']}
 )
